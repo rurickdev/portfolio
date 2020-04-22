883214103ffcc8695fd8a8982e4653dfcbf3c561
Rurick Maqueo Poisot <rurick.mpoisot@gmail.com>
2020-04-22 00:19:00 -0500

    Created index page

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+
+function Portfolio (props) {
+  return (
+    <main className='columns is-centered is-multiline is-marginless is-paddingless is-vcentered'>
+      <div className='colum is-half'>
+        <h1 className='is-size-1'>RurickDev Portfolio [WIP]</h1>
+      </div>
+    </main>
+  )
+}
+
+export default Portfolio
