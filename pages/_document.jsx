bef5e76d1f4cbf518e376901eef5df02e65b74db
Rurick Maqueo Poisot <rurick.mpoisot@gmail.com>
2020-05-03 20:42:54 -0500

    Changed Marial Icons for Fontawesom Icons

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -10,14 +10,12 @@ class MyDocument extends Document {
           <meta httpEquiv='X-UA-Compatible' content='ie=edge' />
           <meta name='theme-color' content='#000000' />
           <link rel='icon' type='image/png' href='https://s3.us-east-2.amazonaws.com/cdn.rurick.dev/assets/img/favicon.png' />
-          {/* Material Icons */}
-          {/* <link rel='stylesheet' href='//cdn.materialdesignicons.com/4.8.95/css/materialdesignicons.min.css' /> */}
-          {/* FontAwesome Icons */}
-          <script src='https://kit.fontawesome.com/846cbbb684.js' crossorigin='anonymous' />
         </Head>
         <body className='has-navbar-fixed-top'>
           <Main />
           <NextScript />
+          {/* FontAwesome Icons */}
+          <script src='https://kit.fontawesome.com/846cbbb684.js' crossOrigin='anonymous' />
         </body>
       </Html>
     )
