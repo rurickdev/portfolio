f64371cbfeae1bd5e5e48fff296435cbdd3626c3
Rurick Maqueo Poisot <rurick.mpoisot@gmail.com>
2020-05-03 20:49:42 -0500

    implemented theme toggler in action bar

--- components/app_bar.jsx
+++ components/app_bar.jsx
@@ -1,17 +1,21 @@
+import DarkModeToggler from './dark_mode_toggler'
 
 function AppBar (props) {
   return (
     <nav className='navbar is-fixed-top is-spaced' role='navigation' aria-label='main navigation'>
 
       <div className='navbar-brand is-size-3'>
+
         <div className='navbar-item' />
-        <span className='is-fira'>
-          Rurick.Dev
-        </span>
+
         <div className='navbar-item'>
-          <span className='icon has-text-primary is-size-4'>
-            <i className='fas fa-moon' />
+          <span className='is-fira'>
+            Rurick.Dev
           </span>
+        </div>
+
+        <div className='navbar-item'>
+          <DarkModeToggler />
         </div>
       </div>
     </nav>
