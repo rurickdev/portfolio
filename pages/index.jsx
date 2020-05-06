b9c953cf339e83a7360d953734c6b4f585adde30
Rurick Maqueo Poisot <rurick.mpoisot@gmail.com>
2020-05-06 01:14:08 -0500

    implemented the header

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,9 @@
+import Header from '../components/header'
 
 function Portfolio (props) {
   return (
-    <main className='columns is-centered is-multiline is-marginless is-paddingless is-vcentered'>
-      <div className='colum is-half'>
-        <h1 className='is-size-1'>RurickDev Portfolio [WIP]</h1>
-      </div>
+    <main>
+      <Header />
     </main>
   )
 }
