c082a03e0397a363f81d1e1c2bccd90a2761dbdf
Rurick Maqueo Poisot <rurick.mpoisot@gmail.com>
2020-05-11 02:37:29 -0500

    added secure url for preview image, alt text and some comments

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -9,12 +9,14 @@ class MyDocument extends Document {
           <meta name='viewport' content='width=device-width, initial-scale=1.0' />
           <meta httpEquiv='X-UA-Compatible' content='ie=edge' />
           <meta name='theme-color' content='#000000' />
+          {/* Fabicon */}
           <link rel='icon' type='image/png' href='https://s3.us-east-2.amazonaws.com/cdn.rurick.dev/assets/img/favicon.png' />
           {/* Web Preview Tags */}
           <meta property='og:title' content='Rurick Dev' />
           <meta property='og:description' content='Full Stack Developer' />
-          <meta property='og:image:secure' content='https://s3.us-east-2.amazonaws.com/cdn.rurick.dev/assets/img/web_preview.png' />
+          <meta property='og:image:secure_url' content='https://s3.us-east-2.amazonaws.com/cdn.rurick.dev/assets/img/web_preview.png' />
           <meta property='og:image' content='http://s3.us-east-2.amazonaws.com/cdn.rurick.dev/assets/img/web_preview.png' />
+          <meta property='og:image:alt' content='The Business card of Rurick Dev' />
           <meta property='og:url' content='https://rurick.dev' />
           <meta name='twitter:card' content='summary_large_image' />
         </Head>
